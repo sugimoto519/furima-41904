274fab2667407e7ce290a19bb6dfff904a58f25b
sugimoto519 <rqsnq531@gmail.com>
2025-03-17 23:04:00 +0900

    ユーザー管理機能の単体テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-        
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/ } do
+    validates :family_name_kanji
+    validates :first_name_kanji
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+
   validates :nickname, presence: true
-  validates :family_name_kanji, presence: true
-  validates :first_name_kanji, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :birthday, presence: true
 end
