2ae6d76b2f875fd140823efcb32590b76b4ac38e
sugimoto519 <rqsnq531@gmail.com>
2025-03-19 23:12:58 +0900

    ユーザー管理機能の修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { Faker::Internet.password(min_length: 6) + '1a'}
     password_confirmation { password }
     family_name_kanji     { '山田' }
     first_name_kanji      { '太郎' }
