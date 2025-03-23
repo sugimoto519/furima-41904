628d4825fa03ee6d06e370c82d8a459ae8441263
sugimoto519 <rqsnq531@gmail.com>
2025-03-23 16:32:58 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,16 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :delivery_day
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :item_text, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_day_id, presence: true
+  validates :price, presence: true
+
+
 end
