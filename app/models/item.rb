ba076e38606705b773360f93a5661b3104ba0e23
sugimoto519 <rqsnq531@gmail.com>
2025-03-23 15:40:15 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :delivery_day
   has_one_attached :image
 end
