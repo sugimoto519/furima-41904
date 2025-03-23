5bd7d7511b6690212131c6827441d42583d9de8b
sugimoto519 <rqsnq531@gmail.com>
2025-03-23 18:19:23 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,12 +10,13 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :item_name, presence: true
   validates :item_text, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :delivery_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :delivery_day_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true
-
+  validates :price, format: { with: /\A[0-9]+\z/ }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
 end
