c03d8da0291ad288dee3b5e7f8b0cbff5beb2187
sugimoto519 <rqsnq531@gmail.com>
2025-04-05 21:13:14 +0900

    プルリクエストによる修正②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,7 +42,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     return if current_user.id == @item.user_id
 
     redirect_to action: :index
