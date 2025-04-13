d28aac36de0bf23f4ea6c5f208f1730da60c6faa
sugimoto519 <rqsnq531@gmail.com>
2025-04-13 16:50:55 +0900

    マイグレーションファイルの実装

--- db/migrate/20250413062942_create_delivery_addresses.rb
+++ db/migrate/20250413062942_create_delivery_addresses.rb
@@ -1,7 +1,13 @@
 class CreateDeliveryAddresses < ActiveRecord::Migration[7.1]
   def change
     create_table :delivery_addresses do |t|
-
+      t.string :postal_code, null: false
+      t.integer :prefecture_id, null: false
+      t.string :city, null: false
+      t.string :block_number, null: false
+      t.string :building_name
+      t.string :phone_number, null: false
+      t.references :purchase_record, null: false, foreign_key: true
       t.timestamps
     end
   end
