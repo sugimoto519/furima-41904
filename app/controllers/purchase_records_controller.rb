e7fdd01bc93c4c00263c5aad9796d42f50d11cee
sugimoto519 <rqsnq531@gmail.com>
2025-04-13 17:41:45 +0900

    link_toによるﾋﾞｭｰﾌｧｲﾙへの遷移実装

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -7,7 +7,7 @@ class PurchaseRecordsController < ApplicationController
     @purchase_record_delivery_address = PurchaseRecordDeliveryAddress.new(purchase_record_delivery_address_params)
     if @purchase_record_delivery_address.valid?
        @purchase_record_delivery_address.save
-       redirect to root_path
+       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
@@ -16,6 +16,6 @@ class PurchaseRecordsController < ApplicationController
   private
 
   def purchase_record_delivery_address_params
-    params.require(:purchase_record_delivery_address).permit(:user_id, :item_id, :postal_code, :prefecture_id, :city, :block_number, :building_name, :phone_number).merge(purchase_record_id: purchase_record.id)
+    params.require(:purchase_record_delivery_address).permit(:postal_code, :prefecture_id, :city, :block_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items do
-    resources :purchase_record, only: [:new, :create]
+    resources :purchase_records, only: [:new, :create]
   end
 end
